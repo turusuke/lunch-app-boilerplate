e10694639298a03f643e36648cb810872d1d1b02
turusuke <mail@turusuke.com>
2020-03-31 03:17:22 +0900

    wip

--- components/counter.tsx
+++ components/counter.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { counterSlice } from '../lib/counter'
+import { counterSlice, fetchTodo as fetchAction } from '../lib/counter'
 
 const useCounter = () => {
   const count = useSelector(state => state.counter)
   const dispatch = useDispatch()
+  const fetchTodo = () => dispatch(fetchAction());
   const increment = () => dispatch(counterSlice.actions.increment())
   const decrement = () => dispatch(counterSlice.actions.decrement())
   const reset = () => dispatch(counterSlice.actions.reset())
-  return { count, increment, decrement, reset }
+  return { count, increment, decrement, reset, fetchTodo }
 }
 
 const Counter = () => {
-  const { count, increment, decrement, reset } = useCounter()
+  const { count, increment, decrement, reset, fetchTodo } = useCounter()
+  fetchTodo()
   return (
     <div>
       <h1>
--- lib/counter.ts
+++ lib/counter.ts
@@ -1,6 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAction, createAsyncThunk } from '@reduxjs/toolkit'
+import ky from 'ky-universal'
 
-export const initialState = 0
+export const initialState = 0 as any
+
+export const fetchTodoThunk = createAsyncThunk(
+  'users/fetchTodo',
+  async () => {
+    const response = await ky.get('https://jsonplaceholder.typicode.com/todos/1').json()
+    console.log(response);
+    return response
+  }
+)
+
 export const counterSlice = createSlice({
   name: 'counter',
   initialState,
@@ -9,5 +20,10 @@ export const counterSlice = createSlice({
     increment: state => state + 1,
     decrement: state => state - 1,
     reset: state => initialState
+  },
+  extraReducers: {
+    [fetchTodoThunk.fulfilled]: ({ state, action }) => {
+      console.log(state)
+    }
   }
 })
